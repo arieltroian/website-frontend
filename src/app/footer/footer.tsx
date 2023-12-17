ef0f538fb25464eaeb7ab3a486a0f4def11d90a2
arieltroian <arieltroian24@gmail.com>
2023-12-17 14:26:38 -0300

    feat: create footer

---
+++ src/app/footer/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="border border-t-ring border-l-transparent border-r-transparent">
+      <div className="lg:px-12 lg:py-6 py-2 flex items-center lg:justify-around">
+        <span className="invisible lg:visible font-semibold">ARIEL </span>
+        <p className="text-sm md:text-base">Feito com 💜 por Ariel Troian</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
